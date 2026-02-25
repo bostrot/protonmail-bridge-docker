056fd6ad267caab37d5d329df425926d8ef55f06
Eric <eric@bostrot.com>
2026-02-25 20:43:08 +0100

    Enhance socat command options for improved connection handling in entrypoint scripts

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -29,8 +29,8 @@ else
     # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
-    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    socat TCP-LISTEN:25,fork,reuseaddr TCP:127.0.0.1:1025,nodelay &
+    socat TCP-LISTEN:143,fork,reuseaddr TCP:127.0.0.1:1143,nodelay &
 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
